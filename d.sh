04af843866d019560443efa85383f031b9506745
enpotid <xian101221@gmail.com>
2025-05-09 18:07:36 +0900

    rebuilding site Fri May  9 06:07:36 PM KST 2025

---
+++ d.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
+
+hugo -t PaperMod
+
+cd public
+git add .
+
+msg="rebuilding site `date`"
+if [ $# -eq 1 ]
+  then msg="$1"
+fi
+git commit -m "$msg"
+
+git push origin gh-pages
+
+cd ..
+
+git add .
+
+msg="rebuilding site `date`"
+if [ $# -eq 1 ]
+  then msg="$1"
+fi
+git commit -m "$msg"
+
+git push origin main
